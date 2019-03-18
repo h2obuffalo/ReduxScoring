5a17c6fcc0d1f7d908daa598a8f06d92d540657f
Aran <h2obuffalo@users.noreply.github.com>
2019-03-18 14:46:28 +0000

    setup state, actions and linked to event handlers

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
+const App = ({handlePlayer1, handlePlayer2, player1Score, player2Score, handleReset}) => (
     <React.Fragment>
         {/* header */}
         <header className="page-header">
@@ -12,15 +12,15 @@ const App = () => (
         {/* scores */}
         <div className="row">
             <div className="col-xs-6">
-                <p>Player 1 <span className="pull-right label label-success">Serving</span></p>
-                <p className="well">{/* player 1 score */}</p>
-                <button className="btn btn-primary">+</button>
+                <p>Player 1 <span className="pull-right label label-success" disabled="true" >Serving</span></p>
+                <p className="well">{/* player 1 score */}{player1Score}</p>
+                <button className="btn btn-primary" onClick={handlePlayer1}>+</button>
             </div>
 
             <div className="col-xs-6">
                 <p>Player 2 <span className="pull-right label label-success">Serving</span></p>
-                <p className="well">{/* player 2 score */}</p>
-                <button className="btn btn-primary">+</button>
+                <p className="well">{/* player 2 score */}{player2Score}</p>
+                <button className="btn btn-primary" onClick={handlePlayer2}>+</button>
             </div>
         </div>
 
@@ -30,7 +30,7 @@ const App = () => (
         <hr />
 
         { /* reset button */}
-        <button className="btn btn-danger">Reset</button>
+        <button className="btn btn-danger" onClick={handleReset} >Reset</button>
     </React.Fragment>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App.jsx';
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const initial = {
+    player1: 0,
+    player2: 0,
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "player1Scores": return {...state, player1: state.player1 + 1};
+        case "player2Scores": return {...state, player2: state.player2 + 1};
+        case "reset": return initial;
+        default: return state;
+    }
+};
+
+const store = createStore(
+    reducer,
+    initial,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    );
+
+// handlePlayer1(){
+
+// }
+
+
+const render = () => {
+  let state = store.getState();
+
+  ReactDOM.render(
+    <App
+        player1Score={state.player1}
+        player2Score={state.player2}
+        handlePlayer1={ () => store.dispatch({type: "player1Scores"})}
+        handlePlayer2={ () => store.dispatch({type: "player2Scores"})}
+        handleReset={ () => store.dispatch({type: "reset"})}
+    />, document.getElementById('root'));
+
+};
+
+store.subscribe(render);
+render();
+
+
